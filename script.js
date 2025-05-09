1b7040ef350aff7a597d682be105fa089d86f769
paritoshdayma <paritosh.dayma9@gmail.com>
2025-05-09 14:41:21 +0530

    complete website paritosh folder upload 9/5/2025

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Lead Product Analyst", "Senior Product Analyst", "Data Analyst", "Product Analyst", "Data Storyteller", "Strategist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true 
+});
